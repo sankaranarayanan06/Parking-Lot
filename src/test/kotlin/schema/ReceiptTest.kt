529066d43b9103e15349dcd8391e7dfa533c1b11
sankar <sankaranarayananm@sahaj.ai>
2023-02-17 10:24:50 +0530

    Update ReceiptTest

--- src/test/kotlin/schema/ReceiptTest.kt
+++ src/test/kotlin/schema/ReceiptTest.kt
@@ -8,7 +8,9 @@ import java.time.LocalDateTime
 class ReceiptTest {
     @Test
     fun `it should generate the receipt`() {
+        val location = Mall()
         val ticket = Ticket(entryDate = LocalDateTime.of(2023, 1, 1, 10, 0, 0), vehicle = CAR)
+        location.unParkVehicle(ticket)
         val receipt = Receipt(exitDate = LocalDateTime.of(2023, 1, 1, 11, 30, 0), ticket = ticket)
         val expectedResponse =
             "Parking Receipt:\n\t" +
