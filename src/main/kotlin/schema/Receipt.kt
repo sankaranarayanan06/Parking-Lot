1a7999b115712408cedb800d05c8abd3d4889b10
sankar <sankaranarayananm@sahaj.ai>
2023-02-17 07:55:12 +0530

    Add: Receipt

--- src/main/kotlin/schema/Receipt.kt
+++ src/main/kotlin/schema/Receipt.kt
@@ -1,4 +1,34 @@
 package schema
 
-class Receipt {
+import java.time.LocalDateTime
+import java.time.temporal.ChronoUnit
+import kotlin.math.ceil
+
+var RECEIPT_NUMBER = 0
+
+class Receipt(
+    private var exitDate: LocalDateTime = LocalDateTime.now(),
+    private var receiptNumber: Int = ++RECEIPT_NUMBER,
+    private var location: Mall = Mall(),
+    private var ticket: Ticket
+) {
+    private var fee: Int = 0
+
+    init {
+        fee = this.location.calculateFee(ticket.getVehicle(), calculateParkedDuration(ticket)).toInt()
+    }
+
+    fun generateReceipt(): String {
+        return "Parking Receipt:\n\t" +
+                "Receipt Number:\t$receiptNumber\n\t" +
+                "Entry Date:\t${ticket.getEntryDate()}\n\t" +
+                "Exit Date:\t${exitDate.withNano(0).toLocalTime()}\n\t" +
+                "Fees:\t$fee\n\t"
+    }
+
+    private fun calculateParkedDuration(ticket: Ticket): Long {
+        val minute = exitDate.minus(ticket.getEntryDate().minute.toLong(), ChronoUnit.MINUTES).minute.toDouble()
+        val hours = exitDate.minus(ticket.getEntryDate().hour.toLong(), ChronoUnit.HOURS).hour.toDouble()
+        return ceil(hours + (minute / 60)).toLong()
+    }
 }
---
+++ src/test/kotlin/schema/ReceiptTest.kt
@@ -0,0 +1,24 @@
+package schema
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import schema.VehicleType.CAR
+import java.time.LocalDateTime
+
+class ReceiptTest {
+    @Test
+    fun `it should generate the receipt`() {
+        val ticket = Ticket(entryDate = LocalDateTime.of(2023, 1, 1, 10, 0, 0), vehicle = CAR)
+        val receipt = Receipt(exitDate = LocalDateTime.of(2023, 1, 1, 11, 30, 0), ticket = ticket)
+        val expectedResponse =
+            "Parking Receipt:\n\t" +
+                    "Receipt Number:\t1\n\t" +
+                    "Entry Date:\t10:00\n\t" +
+                    "Exit Date:\t11:30\n\t" +
+                    "Fees:\t20\n\t"
+
+        val response = receipt.generateReceipt()
+
+        assertEquals(expectedResponse, response)
+    }
+}
