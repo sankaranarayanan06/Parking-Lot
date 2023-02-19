b4cb4a744ed38de6ccd0fb1658a26f6dd672456b
sankar <sankaranarayananm@sahaj.ai>
2023-02-19 14:44:39 +0530

    Add: Singleton object to get instance of ParkingLot

--- src/main/kotlin/model/ParkingLot.kt
+++ src/main/kotlin/model/ParkingLot.kt
@@ -1,17 +1,27 @@
 package model
 
+import exception.SpotAlreadyAcquired
 import java.util.*
+
+object Singleton {
+    private var instance: ParkingLot? = null
+    fun getParkingLotInstance(totalNumberOfSpot: Int): ParkingLot? {
+        instance?.run {
+            println("run - 1")
+            return instance
+        } ?: run {
+            println("run - 2")
+            instance = ParkingLot(totalNumberOfSpot = totalNumberOfSpot)
+            return instance
+        }
+    }
+}
 
 class ParkingLot(
     val totalNumberOfSpot: Int
 ) {
-    private val parkingSpots: List<Boolean>
-    private var numberOfAvailableSpot: Int
-
-    init {
-        parkingSpots = Collections.nCopies(totalNumberOfSpot, true).toMutableList()
-        numberOfAvailableSpot = totalNumberOfSpot
-    }
+    private val parkingSpots = Collections.nCopies(totalNumberOfSpot, true).toMutableList()
+    private var numberOfAvailableSpot: Int = totalNumberOfSpot
 
     fun getFreeSpot(): Int? {
         for (spot in parkingSpots.indices) {
@@ -20,6 +30,14 @@ class ParkingLot(
             }
         }
         return null
+    }
+
+    fun acquireFreeSpot(spot: Int) {
+        if (parkingSpots[spot - 1]) {
+            parkingSpots[spot - 1] = false
+        } else {
+            throw SpotAlreadyAcquired()
+        }
     }
 
     fun isSpotAvailable(): Boolean {
--- src/test/kotlin/model/ParkingLotTest.kt
+++ src/test/kotlin/model/ParkingLotTest.kt
@@ -1,26 +1,49 @@
 package model
 
+import exception.SpotAlreadyAcquired
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import kotlin.test.assertEquals
 
 class ParkingLotTest {
     @Test
     fun `it should check if a free spot is available`() {
-        val parkingLot = ParkingLot(totalNumberOfSpot = 100)
+        val parkingLot = Singleton.getParkingLotInstance(totalNumberOfSpot = 100)
 
-        val response = parkingLot.isSpotAvailable()
+        val response = parkingLot?.run { isSpotAvailable() }
 
-        assertTrue(response)
+        assertTrue(response!!)
     }
 
     @Test
     fun `it should return a free spot`() {
-        val parkingLot = ParkingLot(totalNumberOfSpot = 100)
+        val parkingLot = Singleton.getParkingLotInstance(totalNumberOfSpot = 100)
         val expectedResponse = 1
 
-        val response = parkingLot.getFreeSpot()
+        val response = parkingLot?.run { getFreeSpot() }
 
         assertEquals(expectedResponse, response)
+    }
+
+    @Test
+    fun `it should return the same instance of parkingLot`() {
+        val parkingLot1 = Singleton.getParkingLotInstance(totalNumberOfSpot = 100)
+        val parkingLot2 = Singleton.getParkingLotInstance(totalNumberOfSpot = 100)
+
+        assertEquals(parkingLot1, parkingLot2)
+    }
+
+    @Test
+    fun `it should not allocate an already reserved spot`() {
+        val parkingLot1 = Singleton.getParkingLotInstance(totalNumberOfSpot = 100)
+        parkingLot1?.run { acquireFreeSpot(1) }
+
+        val parkingLot2 = Singleton.getParkingLotInstance(totalNumberOfSpot = 100)
+
+
+        assertThrows<SpotAlreadyAcquired> {
+            parkingLot2?.run { acquireFreeSpot(1) }
+        }
     }
 }
